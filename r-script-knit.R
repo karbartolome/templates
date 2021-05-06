16ba314a010ebd10fd3713c8f705fec4be1ae373
karbartolome <karinabartolome@hotmail.com>
2021-05-06 00:04:20 -0300

    Cajeros automáticos en caba - script r knit

--- r-script-knit.R
+++ r-script-knit.R
@@ -5,9 +5,14 @@
 #'
 #'
 #+ echo=TRUE, fig.width=7, message=FALSE
-#  Comentario
+#  Cajeros automáticos
 library(dplyr)
+library(biscale)
 library(ggplot2)
+library(cowplot)
+library(stringr)
+library(sf)
+
 df <- read.csv('https://cdn.buenosaires.gob.ar/datosabiertos/datasets/cajeros-automaticos/cajeros-automaticos.csv')
 df %>% 
   group_by(banco, red) %>% 
@@ -17,11 +22,61 @@ df %>%
   ggplot(aes(x=reorder(banco,n),y=n, fill=red))+
     geom_col()+
     coord_flip()+
+    scale_fill_manual(values=c('orchid3','turquoise3'))+
     labs(title='Cantidad de cajeros automáticos por banco', 
          subtitle='Datos del GCBA', x='Banco',y='Cantidad de cajeros')+
     theme_minimal()
 
 
-# /* Write comments between /* and */ like C comments:
-Sys.sleep(60)
-# */
+# Mapa barrios CABA
+
+caba <- st_read('http://cdn.buenosaires.gob.ar/datosabiertos/datasets/barrios/barrios.geojson') %>% 
+  mutate(barrio=str_to_title(barrio)) 
+
+# Cantidad de cajeros por barrio
+df <- df %>% filter(localidad=='CABA') %>% 
+  group_by(barrio, red) %>% 
+  summarise(n=n()) %>% 
+  ungroup() %>%
+  filter(barrio!='') %>% 
+  tidyr::pivot_wider(names_from=red, values_from=n) %>% 
+  mutate(BANELCO = tidyr::replace_na(BANELCO,0), 
+         LINK = tidyr::replace_na(LINK,0))
+
+df_barrios <- merge(caba, df, by='barrio', all.x=TRUE, all.y=TRUE)
+
+# Data en formato biscale (quantiles)
+
+data_biscale <- bi_class(df_barrios, 
+                         x='LINK',
+                         y='BANELCO', 
+                         style="quantile",dim=3)
+
+# Mapa con biscale
+mapa <- ggplot() +
+  geom_sf(data = data_biscale, 
+          mapping = aes(fill = bi_class), 
+          color = "white", 
+          size = 0.1, 
+          show.legend = FALSE) +
+  bi_scale_fill(pal = "DkBlue", dim = 3) +
+  labs(title = "Cajeros automáticos",
+       subtitle = "Cantidad de cajeros por red y barrio de CABA", 
+       caption = "Fuente:Gobierno de la Ciudad de Buenos Aires") +
+  bi_theme()+
+  theme(plot.caption = element_text(hjust = 0.2))
+
+# Legend
+legend <- bi_legend(pal = "DkBlue",
+                    dim = 3,
+                    xlab = "LINK",
+                    ylab = "BANELCO",
+                    size = 8)
+
+# Mapa + Legend
+mapa <- ggdraw() +
+  draw_plot(mapa, 0, 0, 1, 1) +
+  draw_plot(legend, 0.63, 0.05, 0.2, 0.2)
+
+#+ echo=TRUE, fig.width=10, message=FALSE
+mapa
